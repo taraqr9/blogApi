869b96767b1bf5580b8b7a182fc3d89ee82aff6a
Md.Taraq Rahman <taraqr9@gail.com>
2022-01-02 14:46:23 +0600

    update github port

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/api/login/github-callback',
+        'redirect' => 'http://localhost:8003/api/login/github-callback',
     ],
 
 ];
